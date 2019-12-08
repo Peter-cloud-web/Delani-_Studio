1ff478f9aa6a0839e1cd215a7ac8784124bcc15a
Peter-cloud-web <petergichia165@gmail.com>
2019-12-08 20:11:36 +0300

    Use click function to hide and show elements upon click

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('img#img2-hide').click(function(){
+    $("p#p2-show").show();
+    $("img#img2-hide").hide();
+  });
+  $('p#p2-show').click(function(){
+    $("p#p2-show").hide();
+    $("img#img2-hide").show();
+  });
+  $('img#img1-hide').click(function(){
+    $("p#p1-show").show();
+    $("img#img1-hide").hide();
+  });
+  $('p#p1-show').click(function(){
+    $("p#p1-show").hide();
+    $("img#img1-hide").show();
+  });
+  $('img#img3-hide').click(function(){
+    $("p#p3-show").show();
+    $("img#img3-hide").hide();
+  });
+  $('p#p3-show').click(function(){
+    $("p#p3-show").hide();
+    $("img#img3-hide").show();
+  });
+});
